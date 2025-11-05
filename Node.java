c32d15e040302bad583c7fba4cae174ad50263f6
CesarV03 <3021762@wcupa.edu>
2025-11-05 00:39:56 -0500

    Node.java

--- Node.java
+++ Node.java
@@ -1,17 +1,29 @@
+import java.util.Random;
+
 public class Node implements Cloneable {
-    private Const constant;
-    private Variable variable;
-    private Binop op;
-    private Node lChild, rChild;
+
+    protected Node lChild;
+    protected Node rChild;
+    protected Binop op;        
+    protected Const c;       
+    protected Variable v;    
 
     public Node(Const c) 
     {
-        this.constant = c;
+        this.c = c;
+        this.v = null;
+        this.op = null;
+        this.lChild = null;
+        this.rChild = null;
     }
 
     public Node(Variable v) 
     {
-        this.variable = v;
+        this.v = v;
+        this.c = null;
+        this.op = null;
+        this.lChild = null;
+        this.rChild = null;
     }
 
     public Node(Binop op, Node left, Node right) 
@@ -19,27 +31,96 @@ public class Node implements Cloneable {
         this.op = op;
         this.lChild = left;
         this.rChild = right;
+        this.c = null;
+        this.v = null;
     }
 
-    public boolean isLeaf() 
+    public Node(Binop op) 
     {
-        return op == null;
+        this.op = op;
+        this.lChild = null;
+        this.rChild = null;
+        this.c = null;
+        this.v = null;
     }
 
     public double eval(double[] data) 
     {
-        if (constant != null) return constant.eval(data);
-        if (variable != null) return variable.eval(data);
-        double left = lChild.eval(data);
-        double right = rChild.eval(data);
-        return op.eval(left, right);
+        if (c != null) return c.eval(data);
+        if (v != null) return v.eval(data);
+        double leftVal = lChild.eval(data);
+        double rightVal = rChild.eval(data);
+        return op.eval(leftVal, rightVal);
     }
 
     public String toString() 
     {
-        if (constant != null) return constant.toString();
-        if (variable != null) return variable.toString();
-        return "(" + lChild.toString() + " " + op.getOp() + " " + rChild.toString() + ")";
+        if (c != null) return c.toString();
+        if (v != null) return v.toString();
+        return "(" + lChild.toString() + " " + op.toString() + " " + rChild.toString() + ")";
+    }
+
+    public void traverse(Collector collector) 
+    {
+        collector.collect(this);
+        if (lChild != null) lChild.traverse(collector);
+        if (rChild != null) rChild.traverse(collector);
+    }
+
+    public void swapLeft(Node trunk) 
+    {
+        Node tmp = this.lChild;
+        this.lChild = trunk.lChild;
+        trunk.lChild = tmp;
+    }
+
+    public void swapRight(Node trunk) 
+    {
+        Node tmp = this.rChild;
+        this.rChild = trunk.rChild;
+        trunk.rChild = tmp;
+    }
+
+    public boolean isLeaf() 
+    {
+        return (op == null);
+    }
+
+    public void addRandomKids(NodeFactory nf, int maxDepth, Random rand) 
+    {
+        if (isLeaf() || maxDepth <= 0) return;
+
+        int numOps = nf.getNumOps();
+        int numVars = nf.getNumIndepVars();
+        int choices = numOps + numVars + 1;
+
+        if (lChild == null) {
+            if (maxDepth == 1) {
+                lChild = nf.getTerminal(rand);
+            } else {
+                int choice = rand.nextInt(choices);
+                if (choice < numOps) {
+                    lChild = nf.getOperator(rand);
+                    lChild.addRandomKids(nf, maxDepth - 1, rand);
+                } else {
+                    lChild = nf.getTerminal(rand);
+                }
+            }
+        }
+
+        if (rChild == null) {
+            if (maxDepth == 1) {
+                rChild = nf.getTerminal(rand);
+            } else {
+                int choice = rand.nextInt(choices);
+                if (choice < numOps) {
+                    rChild = nf.getOperator(rand);
+                    rChild.addRandomKids(nf, maxDepth - 1, rand);
+                } else {
+                    rChild = nf.getTerminal(rand);
+                }
+            }
+        }
     }
 
     public Object clone() 
@@ -49,8 +130,7 @@ public class Node implements Cloneable {
             if (lChild != null) copy.lChild = (Node) lChild.clone();
             if (rChild != null) copy.rChild = (Node) rChild.clone();
             return copy;
-        } catch (CloneNotSupportedException e) 
-        {
+        } catch (CloneNotSupportedException e) {
             return null;
         }
     }
