6f237e70efe90b5be53439bbb5a5ae6e4e397b72
CesarV03 <3021762@wcupa.edu>
2025-11-05 00:30:37 -0500

    GPTree.java

--- GPTree.java
+++ GPTree.java
@@ -1,74 +1,115 @@
 import java.util.*;
 import java.io.*;
 
-public class GPTree implements Comparable<GPTree>, Cloneable {
+public class GPTree implements Collector, Comparable<GPTree>, Cloneable {
 
     private Node root;
+    private ArrayList<Node> crossNodes;
     private double fitness;
 
-  public GPTree(Node root) 
+    public GPTree() 
     {
-        this.root = root;
+        root = null;
     }
 
-    public GPTree(GPTree other) 
+    public GPTree(NodeFactory factory, int maxDepth, Random rand) 
     {
-        this.root = (Node) other.root.clone();
-        this.fitness = other.fitness;
+        root = factory.getOperator(rand);
+        root.addRandomKids(factory, maxDepth, rand);
     }
 
-    public void evalFitness(DataSet dataSet) 
-{
-        double sum = 0.0;
-        for (DataRow row : dataSet.getRows()) {
-            double y = row.getDependentVariable();
-            double[] x = row.getIndependentVariables();
-            double prediction = root.eval(x);
-            sum += Math.pow((prediction - y), 2);
+    @Override
+    public void collect(Node node) 
+    {
+        if (!node.isLeaf()) {
+            crossNodes.add(node);
         }
-        this.fitness = sum;
     }
 
-    public double getFitness() 
+    public void traverse() 
     {
+        crossNodes = new ArrayList<>();
+        root.traverse(this);
+    }
+
+    public String getCrossNodes() 
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < crossNodes.size(); i++) {
+            sb.append(crossNodes.get(i).toString());
+            if (i < crossNodes.size() - 1) sb.append(";");
+        }
+        return sb.toString();
+    }
+
+    public void crossover(GPTree other, Random rand) 
+    {
+        this.traverse();
+        other.traverse();
+        if (this.crossNodes.isEmpty() || other.crossNodes.isEmpty()) return;
+
+        int thisPoint = rand.nextInt(this.crossNodes.size());
+        int otherPoint = rand.nextInt(other.crossNodes.size());
+        boolean left = rand.nextBoolean();
+
+        Node thisTrunk = crossNodes.get(thisPoint);
+        Node otherTrunk = other.crossNodes.get(otherPoint);
+
+        if (left) {
+            thisTrunk.swapLeft(otherTrunk);
+        } else {
+            thisTrunk.swapRight(otherTrunk);
+        }
+    }
+
+    public void evalFitness(DataSet dataSet) {
+        double totalError = 0.0;
+        for (DataRow row : dataSet.getRows()) {
+            double predicted = this.eval(row.getIndependentVariables());
+            double actual = row.getDependentVariable();
+            double diff = predicted - actual;
+            totalError += diff * diff;
+        }
+        fitness = totalError;
+    }
+
+    public double getFitness() {
         return fitness;
     }
 
     @Override
-    public int compareTo(GPTree t) 
-    {
-        if (this.fitness < t.fitness) return -1;
-        else if (this.fitness > t.fitness) return 1;
-        else return 0;
+    public int compareTo(GPTree other) {
+        if (this.fitness < other.fitness) return -1;
+        if (this.fitness > other.fitness) return 1;
+        return 0;
     }
 
     @Override
-    public boolean equals(Object o) 
-    {
+    public boolean equals(Object o) {
         if (o == null || !(o instanceof GPTree)) return false;
         GPTree other = (GPTree) o;
         return this.compareTo(other) == 0;
     }
 
     @Override
-    public Object clone() 
-    {
+    public Object clone() {
         try {
             GPTree copy = (GPTree) super.clone();
-            copy.root = (Node) this.root.clone();
+            if (this.root != null) {
+                copy.root = (Node) this.root.clone();
+            }
             return copy;
         } catch (CloneNotSupportedException e) {
             return null;
         }
     }
 
-    public Node getRoot() 
-    {
-        return root;
+    public double eval(double[] data) {
+        return root.eval(data);
     }
 
-    public String toString() 
-    {
+    @Override
+    public String toString() {
         return root.toString();
     }
 }
