1e15ab42af56944f3cb9f0b9e83f564c93d8e1f6
CesarV03 <3021762@wcupa.edu>
2025-11-05 00:12:39 -0500

    Plus.java

---
+++ Plus.java
@@ -0,0 +1,11 @@
+public class Plus extends Binop {
+    public double eval(double left, double right) 
+    {
+        return left + right;
+    }
+
+    public String toString() 
+    {
+        return "+";
+    }
+}
