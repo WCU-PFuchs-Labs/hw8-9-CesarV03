4e57657c0a109a006d8d5cc44c0b5617322b947c
CesarV03 <3021762@wcupa.edu>
2025-11-05 00:13:50 -0500

    Mult.java

---
+++ Mult.java
@@ -0,0 +1,11 @@
+public class Mult extends Binop {
+    public double eval(double left, double right) 
+    {
+        return left * right;
+    }
+
+    public String toString() 
+    {
+        return "*";
+    }
+}
